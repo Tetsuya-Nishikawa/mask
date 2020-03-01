f748e536604c117427a6abd029379075052fcb2a
Tetsuya-Nishikawa <61189782+Tetsuya-Nishikawa@users.noreply.github.com>
2020-03-01 15:55:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import tensorflow as tf
 
 #3フレームまで全く同じ時系列データdata1, data2
 #4フレーム以降は、異なる。
-#3フレームまでをmaskでTrueとして実験
+#3フレームまでをmaskはTrueとした。
 if __name__ == '__main__':
     data1 = np.random.randn(1, 5, 100, 100, 3)
     mask1 = tf.cast([[True, True, True, False, False]], tf.bool)
 
     #3フレームまでは同じデータを生成する
     data_l = data1[:,0:3]
+    #4フレーム以降のデータを生成
     data_r = np.random.randn(1, 2, 100, 100, 3)
     data2 = np.concatenate([data_l, data_r],1)
     mask2 = tf.cast([[True, True, True, False, False]], tf.bool)
